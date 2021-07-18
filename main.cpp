fed0aa4b92a75c0fa8a5d2a0916d4172a764659b
Chen Yirun 陈奕润 <chenyirun1996@yahoo.com>
2021-07-18 19:22:47 +0800

    lesson 2

--- main.cpp
+++ main.cpp
@@ -7,31 +7,32 @@
 
 const TGAColor white    = TGAColor(255, 255, 255, 255);
 const TGAColor red      = TGAColor(255, 0, 0, 255);
+const TGAColor green    = TGAColor(0, 255, 0, 255);
 Model *model = NULL;
 const int width = 800;
 const int height = 800;
 
-void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color)
+void line(Vec2i v0, Vec2i v1, TGAImage &image, TGAColor color)
 {
     bool steep = false;
-    if (std::abs(y1 - y0) > std::abs(x1 - x0)) // from bottom to top
+    if (std::abs(v1.y - v0.y) > std::abs(v1.x - v0.x)) // from bottom to top
     {
-        std::swap(x0, y0);
-        std::swap(x1, y1);
+        std::swap(v0.x, v0.y);
+        std::swap(v1.x, v1.y);
         steep = true;
     }
-    if (x1 < x0) // from left to right
+    if (v1.x < v0.x) // from left to right
     {
-        std::swap(x1, x0);
-        std::swap(y1, y0);
+        std::swap(v1.x, v0.x);
+        std::swap(v1.y, v0.y);
     }
 
-    int dx = x1 - x0;
-    int dy = y1 - y0;
+    int dx = v1.x - v0.x;
+    int dy = v1.y - v0.y;
     int derror = std::abs(dy);
     int error = 0;
-    int y = y0;
-    for (int x = x0; x <= x1; x++)
+    int y = v0.y;
+    for (int x = v0.x; x <= v1.x; x++)
     {
         if (steep)
             image.set(y, x, color);
@@ -40,8 +41,49 @@ void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color)
         error += derror;
         if (error > dx / 2)
         {
-            y += y1 > y0 ? 1 : -1;
+            y += v1.y > v0.y ? 1 : -1;
             error -= dx;
+        }
+    }
+}
+
+void triangle(Vec2i t0, Vec2i t1, Vec2i t2, TGAImage &image, TGAColor color)
+{
+    if (t0.y > t1.y) std::swap(t0, t1);
+    if (t0.y > t2.y) std::swap(t0, t2);
+    if (t1.y > t2.y) std::swap(t1, t2);
+    
+    int total_height = t2.y - t0.y;
+    for (int y = t0.y; y < t1.y; y++)
+    {
+        int segment_height = t1.y - t0.y + 1; // avoid division by zero
+        float alpha = (y - t0.y) / (float)total_height;
+        float beta = (y - t0.y) / (float)segment_height;
+        Vec2i A = t0 + (t2 - t0) * alpha;
+        Vec2i B = t0 + (t1 - t0) * beta;
+        if (A.x > B.x)
+        {
+            std::swap(A, B);
+        }
+        for (int x = A.x; x <= B.x; x++)
+        {
+            image.set(x, y, color);
+        }
+    }
+    for (int y = t1.y; y < t2.y; y++)
+    {
+        int segment_height = t2.y - t1.y + 1; // avoid division by zero
+        float alpha = (y - t0.y) / (float)total_height;
+        float beta = (y - t1.y) / (float)segment_height;
+        Vec2i A = t0 + (t2 - t0) * alpha;
+        Vec2i B = t1 + (t2 - t1) * beta;
+        if (A.x > B.x)
+        {
+            std::swap(A, B);
+        }
+        for (int x = A.x; x <= B.x; x++)
+        {
+            image.set(x, y, color);
         }
     }
 }
@@ -58,18 +100,24 @@ int main(int argc, char** argv)
     }
 
     TGAImage image(width, height, TGAImage::RGB);
+    Vec3f lightDir(0, 0, -1);
+
     for (int i = 0; i < model->nfaces(); i++)
     {
         std::vector<int> face = model->face(i);
+        Vec2i screen_coords[3];
+        Vec3f world_coords[3];
         for (int j = 0; j < 3; j++)
         {
-            Vec3f v0 = model->vert(face[j]);
-            Vec3f v1 = model->vert(face[(j+1)%3]);
-            int x0 = (v0.x + 1.0) * width / 2.0; // [-1, 1] => [0, width]
-            int y0 = (v0.y + 1.0) * height / 2.0;
-            int x1 = (v1.x + 1.0) * width / 2.0; // [-1, 1] => [0, width]
-            int y1 = (v1.y + 1.0) * height / 2.0;
-            line(x0, y0, x1, y1, image, white);
+            world_coords[j] = model->vert(face[j]);
+            screen_coords[j] = Vec2i((world_coords[j].x + 1) * width / 2, (world_coords[j].y + 1) * height / 2);
+        }
+        Vec3f n = (world_coords[2] - world_coords[0]) ^ (world_coords[1] - world_coords[0]);
+        n.normalize();
+        float intensity = n * lightDir;
+        if (intensity > 0)
+        {
+            triangle(screen_coords[0], screen_coords[1], screen_coords[2], image, TGAColor(intensity * 255, intensity * 255, intensity * 255, 255));
         }
     }
 
