f83997b9dda96c53fa3f2fb97304da7a49bf7460
chenyirun <ericchan1996@qq.com>
2021-07-27 19:30:15 +0800

    chapter 7

--- raytracer/main.cpp
+++ raytracer/main.cpp
@@ -6,6 +6,7 @@
 #include "hittable_list.h"
 #include "sphere.h"
 #include "tgaimage.h"
+#include "camera.h"
 
 color ray_color(const ray &r, const hittable &world) {
     hit_record rec;
@@ -24,6 +25,7 @@ int main() {
     const auto aspect_ratio = 16.0 / 9.0;
     const int image_width = 400;
     const int image_height = static_cast<int>(image_width / aspect_ratio);
+    const int samples_per_pixel = 100;
 
     // World
     hittable_list world;
@@ -31,31 +33,24 @@ int main() {
     world.add(make_shared<sphere>(point3(0,-100.5,-1), 100));
 
     // Camera
-    auto viewport_height = 2.0;
-    auto viewport_width = aspect_ratio * viewport_height;
-    auto focal_length = 1.0;
-
-    auto origin = point3(0, 0, 0);
-    auto horizontal = vec3(viewport_width, 0, 0);
-    auto vertical = vec3(0, viewport_height, 0);
-    auto lower_left_corner = origin - horizontal/2 - vertical/2 - vec3(0, 0, focal_length);
-
+    camera cam;
+    
     // Render
     TGAImage image(image_width, image_height, TGAImage::RGB);
-
-    // std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 
     for (int j = image_height - 1; j >= 0; j--)
     {
         std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
         for (int i = 0; i < image_width; i++)
         {
-            auto u = double(i) / (image_width - 1);
-            auto v = double(j) / (image_height - 1);
-            ray r(origin, lower_left_corner + u*horizontal + v*vertical);
-            color pixel_color = ray_color(r, world);
-            // write_color(std::cout, pixel_color);
-            write_color(image, i, j, pixel_color);
+            color pixel_color(0, 0, 0);
+            for (int s = 0; s < samples_per_pixel; s++) {
+                auto u = (i + random_double()) / (image_width - 1);
+                auto v = (j + random_double()) / (image_height - 1);
+                ray r = cam.get_ray(u, v);
+                pixel_color += ray_color(r, world);
+            }
+            write_color(image, i, j, pixel_color, samples_per_pixel);
         }
     }
     
