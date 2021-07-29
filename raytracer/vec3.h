28dd60481e59a42244a331911b1ca9c3cec8d91d
chenyirun <ericchan1996@qq.com>
2021-07-29 18:05:26 +0800

    fix bug

--- raytracer/vec3.h
+++ raytracer/vec3.h
@@ -16,7 +16,7 @@ class vec3
     double y() const { return e[1]; }
     double z() const { return e[2]; }
 
-    vec3 operator-() const { return vec3(-e[0], -e[1], e[2]); }
+    vec3 operator-() const { return vec3(-e[0], -e[1], -e[2]); }
     double operator[](int i) const { return e[i]; }
     double &operator[](int i) { return e[i]; }
 
