70d0ea71108d2677ecdffa509e0251759e27e226
chenyirun <ericchan1996@qq.com>
2021-07-29 10:27:56 +0800

    chapter 10

--- raytracer/main.cpp
+++ raytracer/main.cpp
@@ -20,6 +20,7 @@ color ray_color(const ray &r, const hittable &world, int depth) {
         color attenuation;
         if (rec.mat_ptr->scatter(r, rec, attenuation, scattered))
             return attenuation * ray_color(scattered, world, depth-1);
+        return color(0,0,0);
     }
     vec3 unit_direction = unit_vector(r.direction());
     auto t = 0.5 * (unit_direction.y() + 1.0);
@@ -39,13 +40,14 @@ int main() {
     hittable_list world;
 
     auto material_ground = make_shared<lambertian>(color(0.8, 0.8, 0.0));
-    auto material_center = make_shared<lambertian>(color(0.7, 0.3, 0.3));
-    auto material_left = make_shared<metal>(color(0.8, 0.8, 0.8), 0.3);
-    auto material_right = make_shared<metal>(color(0.8, 0.6, 0.2), 1.0);
+    auto material_center = make_shared<lambertian>(color(0.1, 0.2, 0.5));
+    auto material_left = make_shared<dielectric>(1.5);
+    auto material_right = make_shared<metal>(color(0.8, 0.6, 0.2), 0.0);
 
     world.add(make_shared<sphere>(point3(   0, -100.5,  -1),  100.0, material_ground));
     world.add(make_shared<sphere>(point3(   0,      0,  -1),    0.5, material_center));
     world.add(make_shared<sphere>(point3(-1.0,      0,  -1),    0.5, material_left));
+    world.add(make_shared<sphere>(point3(-1.0,      0,  -1),   -0.4, material_left));
     world.add(make_shared<sphere>(point3( 1.0,      0,  -1),    0.5, material_right));
 
     // Camera
