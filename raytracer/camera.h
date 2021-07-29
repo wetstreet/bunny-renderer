7d21eefa42b5e9df5730c4c1581d2d9a2bfff501
chenyirun <ericchan1996@qq.com>
2021-07-29 16:43:55 +0800

    chapter 12

--- raytracer/camera.h
+++ raytracer/camera.h
@@ -5,24 +5,29 @@
 
 class camera {
     public:
-        camera(point3 lookfrom, point3 lookat, vec3 vup, double vfov, double aspect_ratio) {
+        camera(point3 lookfrom, point3 lookat, vec3 vup, double vfov, double aspect_ratio, double aperture, double focus_dist) {
             auto theta = degrees_to_radians(vfov);
             auto h = tan(theta / 2);
             auto viewport_height = 2.0 * h;
             auto viewport_width = aspect_ratio * viewport_height;
 
-            auto w = unit_vector(lookfrom - lookat);
-            auto u = unit_vector(cross(vup, w));
-            auto v = cross(w, u);
+            w = unit_vector(lookfrom - lookat);
+            u = unit_vector(cross(vup, w));
+            v = cross(w, u);
 
             origin = lookfrom;
-            horizontal = viewport_width * u;
-            vertical = viewport_height * v;
-            lower_left_corner = origin - horizontal/2 - vertical/2 - w;
+            horizontal = focus_dist * viewport_width * u;
+            vertical = focus_dist * viewport_height * v;
+            lower_left_corner = origin - horizontal/2 - vertical/2 - focus_dist*w;
+
+            lens_radius = aperture / 2;
         }
 
-        ray get_ray(double u, double v) const {
-            return ray(origin, lower_left_corner + u*horizontal + v*vertical - origin);
+        ray get_ray(double s, double t) const {
+            vec3 rd = lens_radius * random_in_unit_disk();
+            vec3 offset = u * rd.x() + v * rd.y();
+
+            return ray(origin + offset, lower_left_corner + s*horizontal + t*vertical - origin - offset);
         }
         
     private:
@@ -30,6 +35,8 @@ class camera {
         point3 lower_left_corner;
         vec3 horizontal;
         vec3 vertical;
+        vec3 u, v, w;
+        double lens_radius;
 };
 
 #endif //__CAMERA_H__
--- raytracer/main.cpp
+++ raytracer/main.cpp
@@ -51,7 +51,13 @@ int main() {
     world.add(make_shared<sphere>(point3( 1.0,      0,  -1),    0.5, material_right));
 
     // Camera
-    camera cam(point3(-2,2,1), point3(0,0,-1), vec3(0,1,0), 20, aspect_ratio);
+    point3 lookfrom(3,3,2);
+    point3 lookat(0,0,-1);
+    vec3 vup(0,1,0);
+    auto dist_to_focus = (lookfrom-lookat).length();
+    auto aperture = 2.0;
+
+    camera cam(lookfrom, lookat, vup, 20, aspect_ratio, aperture, dist_to_focus);
     
     // Render
     TGAImage image(image_width, image_height, TGAImage::RGB);
