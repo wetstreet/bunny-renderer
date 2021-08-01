7b09f69156cbd6bc449bcdf8be05b89ea16328b3
Chen Yirun 陈奕润 <chenyirun1996@yahoo.com>
2021-08-02 00:52:16 +0800

    opengl environment

---
+++ opengl/main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include "glad/glad.h"
+#include "GLFW/glfw3.h"
+
+int main() {
+	glfwInit();
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	GLFWwindow* window = glfwCreateWindow(800, 800, "BunnyRenderer", NULL, NULL);
+	if (window == NULL) {
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+
+	gladLoadGL();
+
+	glViewport(0, 0, 800, 800);
+
+	glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	glfwSwapBuffers(window);
+
+	while (!glfwWindowShouldClose(window)) {
+		glfwPollEvents();
+	}
+
+	glfwDestroyWindow(window);
+	glfwTerminate();
+	return 0;
+}
