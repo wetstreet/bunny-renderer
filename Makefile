5d0f3a05624b91ef6a3dd4b5b95a3160e3d25e18
chenyirun <ericchan1996@qq.com>
2021-08-02 15:42:31 +0800

    imgui

--- Makefile
+++ Makefile
@@ -3,13 +3,20 @@ all: opengl rasterizer raytracer
 	del *.o
 
 opengl:
-	g++ -c src/opengl/glad.c -Iinclude
+	g++ -c include/glad/glad.c -Iinclude
+	g++ -c include/imgui/imgui.cpp -Iinclude
+	g++ -c include/imgui/imgui_demo.cpp -Iinclude
+	g++ -c include/imgui/imgui_draw.cpp -Iinclude
+	g++ -c include/imgui/imgui_impl_glfw.cpp -Iinclude
+	g++ -c include/imgui/imgui_impl_opengl3.cpp -Iinclude
+	g++ -c include/imgui/imgui_tables.cpp -Iinclude
+	g++ -c include/imgui/imgui_widgets.cpp -Iinclude
 	g++ -c src/opengl/main.cpp -Iinclude -o opengl.o
-	g++ -o opengl.exe opengl.o glad.o -Iinclude -Llib -lmingw32 -lglfw3 -lopengl32 -lgdi32 -luser32
+	g++ -o opengl.exe opengl.o glad.o imgui.o imgui_demo.o imgui_draw.o imgui_impl_glfw.o imgui_impl_opengl3.o imgui_tables.o imgui_widgets.o -Iinclude -Llib -lmingw32 -lglfw3 -lopengl32 -lgdi32 -luser32 -limm32
 	del *.o
 
 rasterizer:
-	g++ -c src/tgaimage.cpp -Iinclude
+	g++ -c include/tgaimage.cpp -Iinclude
 	g++ -c src/rasterizer/main.cpp -o rasterizer.o -Iinclude
 	g++ -c src/rasterizer/model.cpp -Iinclude
 	g++ -c src/rasterizer/our_gl.cpp -Iinclude
@@ -18,7 +25,7 @@ rasterizer:
 	del *.o
 
 raytracer:
-	g++ -c src/tgaimage.cpp -Iinclude
+	g++ -c include/tgaimage.cpp -Iinclude
 	g++ -c src/raytracer/main.cpp -o raytracer.o -Iinclude
 	g++ -o raytracer.exe raytracer.o tgaimage.o
 	del *.o
--- src/opengl/main.cpp
+++ src/opengl/main.cpp
@@ -2,6 +2,10 @@
 #include "glad/glad.h"
 #include "GLFW/glfw3.h"
 
+#include "imgui/imgui.h"
+#include "imgui/imgui_impl_glfw.h"
+#include "imgui/imgui_impl_opengl3.h"
+
 int main() {
 	glfwInit();
 
@@ -19,16 +23,73 @@ int main() {
 
 	gladLoadGL();
 
-	glViewport(0, 0, 800, 800);
+	ImGui::CreateContext();
+	ImGui::StyleColorsDark();
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+    ImGui_ImplOpenGL3_Init("#version 150");
 
-	glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
-	glClear(GL_COLOR_BUFFER_BIT);
+    bool show_demo_window = true;
+    bool show_another_window = false;
+    ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
 
-	glfwSwapBuffers(window);
 
-	while (!glfwWindowShouldClose(window)) {
+	while (!glfwWindowShouldClose(window))
+	{
 		glfwPollEvents();
+
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+		
+        // 1. Show the big demo window (Most of the sample code is in ImGui::ShowDemoWindow()! You can browse its code to learn more about Dear ImGui!).
+        if (show_demo_window)
+            ImGui::ShowDemoWindow(&show_demo_window);
+
+        // 2. Show a simple window that we create ourselves. We use a Begin/End pair to created a named window.
+        {
+            static float f = 0.0f;
+            static int counter = 0;
+
+            ImGui::Begin("Hello, world!");                          // Create a window called "Hello, world!" and append into it.
+
+            ImGui::Text("This is some useful text.");               // Display some text (you can use a format strings too)
+            ImGui::Checkbox("Demo Window", &show_demo_window);      // Edit bools storing our window open/close state
+            ImGui::Checkbox("Another Window", &show_another_window);
+
+            ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
+            ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
+
+            if (ImGui::Button("Button"))                            // Buttons return true when clicked (most widgets return true when edited/activated)
+                counter++;
+            ImGui::SameLine();
+            ImGui::Text("counter = %d", counter);
+
+            ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+            ImGui::End();
+        }
+
+        // 3. Show another simple window.
+        if (show_another_window)
+        {
+            ImGui::Begin("Another Window", &show_another_window);   // Pass a pointer to our bool variable (the window will have a closing button that will clear the bool when clicked)
+            ImGui::Text("Hello from another window!");
+            if (ImGui::Button("Close Me"))
+                show_another_window = false;
+            ImGui::End();
+        }
+
+        ImGui::Render();
+		glViewport(0, 0, 800, 800);
+		glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+		glfwSwapBuffers(window);
 	}
+
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
 
 	glfwDestroyWindow(window);
 	glfwTerminate();
