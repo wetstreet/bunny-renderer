4cbc00cd226ef2161876c2d6d9eafed508c36354
Chen Yirun 陈奕润 <chenyirun1996@yahoo.com>
2021-08-29 00:10:07 +0800

    mouse click select

--- src/opengl/Camera.cpp
+++ src/opengl/Camera.cpp
@@ -9,9 +9,6 @@ Camera::Camera(int width, int height, glm::vec3 position)
 
 void Camera::updateMatrix(float FOVdeg, float nearPlane, float farPlane, float width, float height)
 {
-    glm::mat4 view = glm::mat4(1.0f);
-    glm::mat4 projection = glm::mat4(1.0f);
-
     view = glm::lookAt(Position, Position + Orientation, Up);
     projection = glm::perspective(glm::radians(FOVdeg), (float)(width / height), nearPlane, farPlane);
 
--- src/opengl/Camera.h
+++ src/opengl/Camera.h
@@ -18,6 +18,8 @@ class Camera
         glm::vec3 Orientation = glm::vec3(0.0f, 0.0f, -1.0f);
         glm::vec3 Up = glm::vec3(0.0f, 1.0f, 0.0f);
         glm::mat4 cameraMatrix = glm::mat4(1.0f);
+        glm::mat4 view = glm::mat4(1.0f);
+        glm::mat4 projection = glm::mat4(1.0f);
 
         glm::vec2 windowPos;
         glm::vec2 viewport;
